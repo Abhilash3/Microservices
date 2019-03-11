83573292ac41235fccc33bd5205fc3be3036362f
Abhilash Vedwan <abhi211193@gmail.com>
2019-03-11 18:19:16 +0530

    <Abhilash 3/11/2019> Week 3 Tasks.
    Added unit tests for controller and service.

--- src/test/java/com/epam/onboarding/controller/ReviewControllerTest.java
+++ src/test/java/com/epam/onboarding/controller/ReviewControllerTest.java
@@ -8,18 +8,29 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ReviewController.class)
 @ActiveProfiles("test")
 public class ReviewControllerTest {
+
+    private static final String REVIEW_DESCRIPTION_EXPRESSION = "$.description";
 
     @Autowired
     private MockMvc mvc;
@@ -31,30 +42,89 @@ public class ReviewControllerTest {
     private IReviewService reviewService;
 
     @Test
-    public void fetchProduct() throws Exception {
+    public void fetchReview() throws Exception {
+        Review review = review("ok", 3);
+
+        when(reviewService.getById(review.getReviewId())).thenReturn(review);
+
+        mvc.perform(get(String.format("/%d/reviews/%d", review.getProductId(), review.getReviewId())).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(REVIEW_DESCRIPTION_EXPRESSION, is(review.getDescription())));
+        verify(reviewService).getById(review.getReviewId());
     }
 
     @Test
-    public void fetchProducts() throws Exception {
+    public void fetchReviews() throws Exception {
+        List<Review> reviews = Arrays.asList(review(100L, "ok", 3), review(110L, "bad", 2), review(120L, "good", 4));
+
+        when(reviewService.getAllForProduct(10L)).thenReturn(reviews);
+
+        mvc.perform(get("/10/reviews").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].description", is(reviews.get(0).getDescription())))
+                .andExpect(jsonPath("$[1].description", is(reviews.get(1).getDescription())))
+                .andExpect(jsonPath("$[2].description", is(reviews.get(2).getDescription())));
+        verify(reviewService).getAllForProduct(10L);
     }
 
     @Test
-    public void createProduct() throws Exception {
+    public void createReview() throws Exception {
+        Review review = review(null, 20L, "bad", 2);
+
+        when(reviewService.save(any(Review.class))).thenReturn(review);
+
+        mvc.perform(
+                post("/20/reviews")
+                        .param("description", review.getDescription())
+                        .param("rating", String.valueOf(review.getRating()))
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(REVIEW_DESCRIPTION_EXPRESSION, is(review.getDescription())));
+
+        verify(reviewService).save(eq(review));
     }
 
     @Test
-    public void createDuplicateProduct() throws Exception {
+    public void updateReview() throws Exception {
+        Review review = review("v.good", 5);
+
+        when(reviewService.getById(review.getReviewId())).thenReturn(review);
+
+        mvc.perform(
+                put(String.format("/%d/reviews/%d", review.getProductId(), review.getReviewId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"property\": \"description\", \"value\": \"very good\"}")
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(REVIEW_DESCRIPTION_EXPRESSION, is("very good")));
+
+        verify(reviewService).getById(review.getProductId());
+        verify(reviewService).save(eq(review));
     }
 
     @Test
-    public void updateProduct() throws Exception {
+    public void deleteReview() throws Exception {
+        Review review = review("bad", 2);
+
+        when(reviewService.removeById(review.getReviewId())).thenReturn(review);
+
+        mvc.perform(delete(String.format("/%d/reviews/%d", review.getProductId(), review.getReviewId())).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(REVIEW_DESCRIPTION_EXPRESSION, is(review.getDescription())));
+
+        verify(reviewService).removeById(review.getReviewId());
     }
 
-    @Test
-    public void deleteProduct() throws Exception {
+    private Review review(String description, int rating) {
+        return review(10L, description, rating);
     }
 
-    private Review review(String description) {
-        return new Review().setDescription(description).setReviewId(100L);
+    private Review review(Long reviewId, String description, int rating) {
+        return review(reviewId, 10L, description, rating);
+    }
+
+    private Review review(Long reviewId, Long productId, String description, int rating) {
+        return new Review().setReviewId(reviewId).setProductId(productId).setDescription(description).setRating(rating);
     }
 }
--- src/test/java/com/epam/onboarding/service/ReviewServiceTest.java
+++ src/test/java/com/epam/onboarding/service/ReviewServiceTest.java
@@ -1,6 +1,7 @@
 package com.epam.onboarding.service;
 
 import com.epam.onboarding.dao.ReviewDAO;
+import com.epam.onboarding.domain.Review;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,26 +29,77 @@ public class ReviewServiceTest {
     private ReviewDAO reviewDAO;
 
     @Test
-    public void saveProduct() {
+    public void saveReview() {
+        when(reviewDAO.findAll()).thenReturn(Collections.emptyList());
+
+        Review review = review("ok", 3);
+        reviewService.save(review);
+
+        verify(reviewDAO).save(review);
     }
 
     @Test
-    public void findAllProducts() {
+    public void findAllReviews() {
+        List<Review> reviews = Arrays.asList(review(100L, "ok", 3), review(110L, "bad", 2), review(120L, "good", 4));
+        when(reviewDAO.findAll()).thenReturn(reviews);
+
+        assertEquals(reviews, reviewService.getAll());
+        verify(reviewDAO).findAll();
     }
 
     @Test
-    public void findProductByName() {
+    public void findReviewsByProduct() {
+        List<Review> reviews = Arrays.asList(review(100L, 10L, "ok", 3), review(110L, 20L, "bad", 2), review(120L, 10L, "good", 4));
+        when(reviewDAO.findAll()).thenReturn(reviews);
+
+        List<Review> productReviews = Arrays.asList(review(100L, 10L, "ok", 3), review(120L, 10L, "good", 4));
+        assertEquals(productReviews, reviewService.getAllForProduct(10L));
+        verify(reviewDAO).findAll();
     }
 
     @Test
-    public void findProductById() {
+    public void findReviewById() {
+        List<Review> reviews = Arrays.asList(review(100L, "ok", 3), review(110L, "bad", 2), review(120L, "good", 4));
+        reviews.forEach(review -> when(reviewDAO.findOne(review.getReviewId())).thenReturn(review));
+
+        assertEquals(review(100L, "ok", 3), reviewService.getById(100L));
+        assertEquals(review(110L, "bad", 2), reviewService.getById(110L));
+
+        verify(reviewDAO, times(2)).findOne(anyLong());
     }
 
     @Test
-    public void removeProduct() {
+    public void removeReview() {
+        Review review = review("bad", 2);
+        assertNotNull(review.getProductId());
+
+        reviewService.remove(review);
+
+        verify(reviewDAO).delete(review);
+        assertNull(review.getReviewId());
     }
 
     @Test
-    public void removeProductById() {
+    public void removeReviewById() {
+        Review review = review(200L, "v.good", 5);
+
+        when(reviewDAO.findOne(review.getReviewId())).thenReturn(review);
+
+        reviewService.removeById(200L);
+
+        verify(reviewDAO).findOne(200L);
+        verify(reviewDAO).delete(200L);
+    }
+
+    private Review review(String description, int rating) {
+        return review(10L, description, rating);
+    }
+
+    private Review review(Long reviewId, String description, int rating) {
+        return review(reviewId, 10L, description, rating);
+    }
+
+    private Review review(Long reviewId, Long productId, String description, int rating) {
+        return new Review().setReviewId(reviewId).setProductId(productId).setDescription(description).setRating(rating);
     }
 }
