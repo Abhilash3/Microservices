f15160fac0f3fd154fae66055df63d1b12b8d413
Abhilash Vedwan <abhi211193@gmail.com>
2019-05-12 17:47:47 +0530

    <Abhilash 5/12/2019> Allowed all methods from all urls because of CORS issue.
    Fixed gateway port as 8080 for client communication.

--- src/main/java/com/epam/onboarding/Application.java
+++ src/main/java/com/epam/onboarding/Application.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static java.util.Arrays.stream;
 
 @EnableZuulProxy
 @EnableEurekaClient
@@ -20,5 +25,16 @@ public class Application {
     @Bean
     public ZuulFilter basicFilter() {
         return new LoggerFilter();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods(stream(HttpMethod.values()).map(Enum::name).toArray(String[]::new));
+            }
+        };
     }
 }
